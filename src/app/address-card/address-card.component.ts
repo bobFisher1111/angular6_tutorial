b80a22a1031ef2e0430a22d8b05ab8d8bf35c8c3
bobFisher1111 <rroseberry@twu.edu>
2020-09-09 19:12:58 -0700

    Update

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-address-card',
@@ -8,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class AddressCardComponent implements OnInit {
 
   user: any;
+  // this will hold the value that will be passed in from app.component.html, using @Input annotation
+  @Input('name') userName: string; // must specify in the Input property passing in from, decalring it with @Input
 
-  constructor() { 
+  constructor() {}
 
+  ngOnInit(): void {
     this.user = {
-      name: 'Bob',
+      name: this.userName,
       title: 'Fisherman',
       address: '1616 Fuqua Dr, Flower Mound, Tx 75028',
       // Loop through this part in the html file with ngFor
@@ -21,9 +24,6 @@ export class AddressCardComponent implements OnInit {
         '111-111-1111'
       ]
     };
-  }
-
-  ngOnInit(): void {
   }
 
 }
